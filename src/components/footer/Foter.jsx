cd4d29d28c132349b494a8db63808a3a9dc3b898
Alexey <alexeyslbz@gmail.com>
2023-03-28 00:26:34 +0400

    first commit

---
+++ src/components/footer/Foter.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Reviews from "../reviews/Reviews";
+
+
+function Foter (){
+
+  return(
+    <section>
+      <Reviews/>
+    </section>
+  )
+}
+
+export default Foter;
